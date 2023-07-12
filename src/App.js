b4860fab17c3b59256f3710321b754771c29e74d
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-07-12 18:39:22 +0300

    güncellendi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
-
 import MovieHeader from './components/MovieHeader';
-
 import FavoriteMovieList from './components/FavoriteMovieList';
+import EditMovieForm from "./components/EditMovieForm";
+import AddMovieForm from "./components/AddMovieForm";
 
 import axios from 'axios';
+import DarkMode from "./components/DarkMode";
+
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const history = useHistory(); 
+  const [darkMode, setDarkMode] = useState(false); 
 
   useEffect(() => {
     axios.get('http://localhost:9000/api/movies')
@@ -25,16 +29,28 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id) => {
+    axios
+      .delete(`http://localhost:9000/api/movies/${id}`)
+      .then(res => {
+        setMovies(res.data)
+        history.push("/movies")
+      })
+      .catch(err => console.log(err))
   }
 
   const addToFavorites = (movie) => {
-
+    if(!favoriteMovies.find(fav => fav.id === movie.id)){
+      return setFavoriteMovies([...favoriteMovies, movie])
+    }
   }
+   
 
   return (
-    <div>
+    <div className={`${darkMode ? "dark bg-slate-900" : "light" }`}>
+      <DarkMode darkMode={darkMode} setDarkMode={setDarkMode} />
       <nav className="bg-zinc-800 px-6 py-3">
         <h1 className="text-xl text-white">HTTP / CRUD Film Projesi</h1>
+ 
       </nav>
 
       <div className="max-w-4xl mx-auto px-3 pb-4">
@@ -44,10 +60,15 @@ const App = (props) => {
 
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies} />
+            </Route>
+
+            <Route path="/movies/add">
+            <AddMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie />
+              <Movie deleteMovie={deleteMovie} addToFavorites={addToFavorites} />
             </Route>
 
             <Route path="/movies">
